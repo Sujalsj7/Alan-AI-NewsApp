741fdae6a84b4d205fc30f5e4068f3ded4974579
sujal jaiswal <sujaljaiswalsj07@gmail.com>
2022-01-23 17:42:49 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
 
-function App() {
+import "./App.css";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import React, { useState, useEffect } from 'react';
+
+
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from './styles.js';
+import wordsToNumbers from 'words-to-numbers';
+// import { Typography } from '@material-ui/core';
+
+const alanKey =
+  "0bf46c01ea764430d63711f3f4b33fd62e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg"
+          className={classes.alanLogo}
+          alt="logo"
+        />
+      </div>
+      <NewsCards articles={newsArticles} />
     </div>
   );
-}
+};
 
 export default App;
